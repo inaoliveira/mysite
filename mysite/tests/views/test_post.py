1bf337e58c5aa87dcabbc88001f87f033ae5b885
inacio oliveira <inaciocardoso2007@hotmail.com>
2024-08-05 22:01:04 -0300

    Adiciona testes para views e corrige importação de módulos

---
+++ mysite/tests/views/test_post.py
@@ -0,0 +1,18 @@
+import pytest
+import pytest
+
+
+
+from django.urls import reverse
+
+@pytest.mark.django_db
+def teste_post_view(client):
+    url = reverse('home')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.content == b'Hello World'
+
+    
+
+    
+    
